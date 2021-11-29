f22b46a8d3a1feb88102e889125ba49ae0eb3af7
monaSalih <monaalobisat@gmail.com>
2021-11-29 19:35:30 +0200

    ..

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # [pull_request](https://github.com/monaSalih/cookie-stand-admin/pull/1)
+
+
+sorry if the lab didnt complete but i face broblem with yarn so i cant render the app
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,61 @@
 import Head from 'next/head'
 import {useState} from 'react'
 export default function Home() {
+  const[storeData,setstoreData] = useState([]);
+  
+  function eventHandler(event){
+    event.preventDefault();
+    let shopData={
+      location:event.target.location.value,
+      minCustomer: event.target.min.value,
+      maxCustomer:event.target.max.value,
+      avgCookies:event.target.ave.value,
+
+    }
+    setstoreData(shopData)
+  }
   return (
-    const[storeDa]
+    <div className="">
+      <Head className=""> <title>cookie-stand-admin
+        </title> </Head>
+
+        <header className="">
+          <h1>
+          cookie-stand-admin
+          </h1>
+
+        </header>
+
+        <main className="">
+          <from className="" onSubmit={eventHandler}>
+            <div>
+              <label> Location </label>
+              <input name="loc"/>
+            </div>
+            <div>
+            {/* Maximum Customers per Hour Average Cookies per Sale */}
+              <label> Minimum Customers per Hour </label>
+              <input name="loc"/>
+            </div>
+
+            <div>
+            {/* Average Cookies per Sale */}
+              <label> Maximum Customers per Hour  </label>
+              <input name="loc"/>
+            </div>
+
+            <div>
+            {/*  */}
+              <label> Average Cookies per Sale  </label>
+              <input name="loc"/>
+            </div>
+          </from>
+        </main>
+        <footer>
+          &copy;2021
+        </footer>
+       
+    </div>
+
   )
 }
